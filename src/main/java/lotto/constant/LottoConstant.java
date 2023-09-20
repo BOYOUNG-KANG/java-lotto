4e5743d2c2ea4fa521a5a5c620ea8bea7e6bd2cc
BOYOUNG-KANG <28287@naver.com>
2023-09-20 19:15:58 +0900

    feat(LottoConstant): 상금 상수 추가

--- src/main/java/lotto/constant/LottoConstant.java
+++ src/main/java/lotto/constant/LottoConstant.java
@@ -5,4 +5,9 @@ public class LottoConstant {
     public static final int LOTTO_MIN_RANGE = 1;
     public static final int LOTTO_MAX_RANGE = 45;
     public static final int LOTTO_COUNT = 6;
+    public static final Integer REWARD_THREE = 5000;
+    public static final Integer REWARD_FOUR = 50000;
+    public static final Integer REWARD_FIVE = 1500000;
+    public static final Integer REWARD_FIVE_AND_BONUS = 30000000;
+    public static final Integer REWARD_SIX = 2000000000;
 }

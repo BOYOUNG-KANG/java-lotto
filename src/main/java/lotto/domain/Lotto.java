3eaf8e28170252dc3ac7e3a7f897c997378151d5
BOYOUNG-KANG <28287@naver.com>
2023-09-20 16:18:02 +0900

    feat(Lotto): 로또 번호 저장 로직 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private List<List<Integer>> lottos;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+    }
+
+    public void updateLottos(List<List<Integer>> lottos) {
+        this.lottos = lottos;
     }
 
     private void validate(List<Integer> numbers) {

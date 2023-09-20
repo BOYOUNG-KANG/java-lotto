8826954062fc1961f3dedbc92b8a89820435c1ff
BOYOUNG-KANG <28287@naver.com>
2023-09-20 17:50:47 +0900

    feat(Lotto): 보너스 번호 예외 처리 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -10,6 +10,7 @@ public class Lotto {
     private final List<Integer> numbers;
     private int bonusNumber;
     private List<List<Integer>> lottos;
+    private Set<Integer> duplicatedCheckBox = new HashSet<>();
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -28,17 +29,29 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) throw new IllegalArgumentException(ValidateErrorMessage.LUCKY_COUNT_ERROR);
 
-        Set<Integer> duplicatedCheckBox = new HashSet<>();
         for (int i = 0; i < numbers.size(); i++) {
             Integer number = numbers.get(i);
-            if(number < 1 || number > 45) throw new IllegalArgumentException(ValidateErrorMessage.RANGE_ERROR);
+            if(isOutOfRange(number)) throw new IllegalArgumentException(ValidateErrorMessage.RANGE_ERROR);
             duplicatedCheckBox.add(number);
         }
-        if (duplicatedCheckBox.size() != 6) throw new IllegalArgumentException(ValidateErrorMessage.DUPLICATE_ERROR);
+        if (isDuplicated()) throw new IllegalArgumentException(ValidateErrorMessage.DUPLICATE_ERROR);
     }
+
+    private boolean isDuplicated() {
+        return duplicatedCheckBox.size() != 6;
+    }
+
     private void validate(int bonusNumber) {
-
+        duplicatedCheckBox.add(bonusNumber);
+        if (isOutOfRange(bonusNumber)) throw new IllegalArgumentException(ValidateErrorMessage.RANGE_ERROR);
+        if (isDuplicatedWithNumbers()) throw new IllegalArgumentException(ValidateErrorMessage.DUPLICATE_ERROR);
     }
 
-    // TODO: 추가 기능 구현
+    private boolean isDuplicatedWithNumbers() {
+        return duplicatedCheckBox.size() != 7;
+    }
+
+    private static boolean isOutOfRange(Integer number) {
+        return number < 1 || number > 45;
+    }
 }

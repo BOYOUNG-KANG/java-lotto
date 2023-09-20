5b2953df84837b71467c3910dd5832e5182b19ad
BOYOUNG-KANG <28287@naver.com>
2023-09-20 16:49:08 +0900

    feat(ValidateErrorMessage): 당첨번호와 보너스 번호 입력값 예외 처리 메시지 추가

--- src/main/java/lotto/messages/ValidateErrorMessage.java
+++ src/main/java/lotto/messages/ValidateErrorMessage.java
@@ -2,4 +2,11 @@ package lotto.messages;
 
 public class ValidateErrorMessage {
     public static final String PAYMENT_ERROR = "[ERROR] 구입 금액은 1,000원 단위여야 합니다.";
+    public static final String RANGE_ERROR = "[ERROR] 로또 번호의 숫자 범위는 1~45까지 입니다.";
+    public static final String LUCKY_COUNT_ERROR = "[ERROR] 당첨 번호는 6개의 숫자를 선택해야 합니다.";
+
+    public static final String BONUS_COUNT_ERROR = "[ERROR] 보너스 번호는 1개의 숫자를 선택해야 합니다.";
+
+    public static final String DUPLICATE_ERROR = "[ERROR] 서로 다른 숫자여야 합니다.";
+
 }
